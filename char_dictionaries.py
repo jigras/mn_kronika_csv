29ddfb4da692ea1c2a2eb7b9a2b2567a92b0f648
Jaroslaw <jarekigras@gmail.com>
2020-09-03 12:02:14 +0200

    v0.01

---
+++ char_dictionaries.py
@@ -0,0 +1,89 @@
+from collections import UserDict
+
+
+class CharDict(UserDict):
+    def show_chars(self):
+        print(self.values())
+
+
+polish_char_dict = {
+    b'E\x1b': b'\xc5\x9b',
+    b'E:': b'\xc5\xba',
+    b'E;': b'\xc5\xbb',
+    b'E9': b'\xc5\xb9',
+    b'E<': b'\xc5\xbc',
+    b'E\x1a': b'\xc5\x9a',
+    b'E\x01': b'\xc5\x81',
+    b'E\x02': b'\xc5\x82',
+    b'E\x03': b'\xc5\x83',
+    b'E\x04': b'\xc5\x84',
+    b'D\x07': b'\xc4\x87',
+    b'D\x08': b'\xc4\x88',
+}
+
+czech_char_dict = {
+    b'D\x0c': b'\xc4\x8c',
+    b'D\x0d': b'\xc4\x8d',
+    b'D<': b'\xc4\xbc',
+    b'D>': b'\xc4\xbe'
+
+}
+
+latin_char_dict = {
+    b'C<': b'\xc3\xbc',
+    b'C>': b'\xc3\xbe',
+    b'C\x06': b'\xc3\x86',
+    b'C\x16': b'\xc3\x96',
+    b'aL\x08': b'a\xcc\x88',
+}
+special_char_dict = {
+    b'b\u13\u13': b'\xe2\x80\x93',
+    b'b\x00\x13': b'\xe2\x80\x93',
+    b'b\x00\x1d': b'\xe2\x80\x9d',
+    b'b\x00\x1e': b'\xe2\x80\x9e',
+    b'\x00\x19': b'\xe2\x80\x99',
+    b'b\x00&': b'\xe2\x80\xa6',
+    b'b\x00\x12': b'\xe2\x80\x92',
+    b'b\x00\x14': b'\xe2\x80\x94',
+    b'b\x00\x11': b'\xe2\x80\x93',
+    b'b\x00\x1c': b'\xe2\x80\x9c'
+
+}
+
+redundant_char = {
+    b'o;?': b''
+}
+
+cyrilic_char_dict = {
+    b'Q\x00': b'\xd1\x80',
+    b'Q\x01': b'\xd1\x81',
+    b'Q\x02': b'\xd1\x82',
+    b'Q\x03': b'\xd1\x83',
+    b'Q\x04': b'\xd1\x84',
+    b'Q\x05': b'\xd1\x85',
+    b'Q\x06': b'\xd1\x86',
+    b'Q\x0c': b'\xd1\x8c',
+    b'P:': b'\xd0\xba',
+    b'P;': b'\xd0\xbb',
+    b'P<': b'\xd0\xbc',
+    b'P>': b'\xd0\xbe',
+    b'P?': b'\xd0\xbf',
+    b'P!': b'\xd0\xa1',
+    b'P0': b'\xd0\xb0',
+    b'P1': b'\xd0\xb1',
+    b'P2': b'\xd0\xb2',
+    b'P3': b'\xd0\xb3',
+    b'P4': b'\xd0\xb4',
+    b'P5': b'\xd0\xb5',
+    b'P6': b'\xd0\xb6',
+    b'P7': b'\xd0\xb7',
+    b'P8': b'\xd0\xb8',
+    b'P9': b'\xd0\xb9',
+
+}
+
+corrupted_char ={
+    b'aL\n': b'a\xcc\x88'
+}
+
+
---
+++ main.py
@@ -0,0 +1,61 @@
+import csv
+from io import StringIO
+from char_dictionaries import polish_char_dict, czech_char_dict, latin_char_dict, special_char_dict, redundant_char, \
+    cyrilic_char_dict, corrupted_char
+
+
+def re_encode(byte_conv, conv_dict):
+    """
+    Converts characters in the supplied bytes specified in dictionary keys with values from the dictionary
+    :param byte_onv:
+    :param conv_dict:
+    :return: byte
+    """
+    for key in conv_dict.keys():
+        byte_conv = byte_conv.replace(key, conv_dict[key])
+    return byte_conv
+
+
+def csv_converter(filepath):
+    """
+    Converts corrupted csv from National Museum Kraków to UTF-8 comma separated csv file
+    :param filepath: str
+    :return:
+    """
+    error_counter = 0
+    all_dicts = {**polish_char_dict,
+                 **special_char_dict,
+                 **redundant_char,
+                 **cyrilic_char_dict,
+                 **czech_char_dict,
+                 **latin_char_dict
+                 }
+
+    with open(filepath, 'rb') as csv_file:
+        temp_byte = b''
+        csv_lines = csv_file.readlines()
+        for i, value in enumerate(csv_lines):
+            repaired_bin = re_encode(value, all_dicts)
+            # when ä occurs it
+            if repaired_bin.find(b'aL\n') != -1:
+                temp_byte = re_encode(repaired_bin, corrupted_char)
+                continue
+            if temp_byte:
+                repaired_bin = temp_byte + repaired_bin
+            temp_byte = b''
+            repaired_string = repaired_bin.decode('UTF-8')
+            csv_file_io = StringIO(repaired_string)
+            csv_reader = csv.reader(csv_file_io, delimiter=';')
+            with open('csv_encoded.csv', 'a') as new_file:
+                csv_writer = csv.writer(new_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                try:
+                    csv_writer.writerow(list(csv_reader)[0])
+                except csv.Error as e:
+                    print(e)
+                    error_counter += 1
+
+    if error_counter:
+        print('{} Errors'.format(error_counter))
+
+
+csv_converter('file.csv')
