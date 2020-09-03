a42cfa38dbd063dc57581fbb6ed18abcf2ffadbb
Jaroslaw <jarekigras@gmail.com>
2020-09-03 17:37:47 +0200

    Logger added

--- mn_converter.py
+++ mn_converter.py
@@ -1,9 +1,18 @@
+import logging
 import csv
 import argparse
 from io import StringIO
 
 from char_dictionaries import polish_char_dict, czech_char_dict, latin_char_dict, special_char_dict, redundant_char, \
     cyrilic_char_dict, corrupted_char
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('convert.log')
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(logging.StreamHandler())
 
 
 def re_encode(byte_conv, conv_dict):
@@ -24,16 +33,30 @@ def write_line_to_csv(line_to_add):
     :param line_to_add: line to add
     :return: bool
     """
-
     saved = True
     with open('csv_encoded.csv', 'a') as new_file:
         csv_writer = csv.writer(new_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         try:
             csv_writer.writerow(line_to_add)
         except csv.Error as e:
-            print(e)
+            logger.error(e, exc_info=True)
             saved = False
     return saved
+
+
+def stream_lines(file_path):
+    """
+    Splits file into smaller parts, yields file lines
+    :param file_name: filepath
+    :return: bytes
+    """
+    file = open(file_path, 'rb')
+    while True:
+        line = file.readline()
+        if not line:
+            file.close()
+            break
+        yield line
 
 
 def csv_converter(filepath):
@@ -53,45 +76,37 @@ def csv_converter(filepath):
                  **latin_char_dict
                  }
 
-    show_input(filepath)
-
-    with open(filepath, 'rb') as csv_file:
+    logger.info('File to convert: {}'.format(filepath))
+    temp_byte = b''
+    for value in stream_lines(filepath):
+        repaired_bin = re_encode(value, all_dicts)
+        # when ä occurs (aL\n)
+        if repaired_bin.find(b'aL\n') != -1:
+            temp_byte = re_encode(repaired_bin, corrupted_char)
+            continue
+        if temp_byte:
+            repaired_bin = temp_byte + repaired_bin
         temp_byte = b''
-        csv_lines = csv_file.readlines()
-        for i, value in enumerate(csv_lines):
-            repaired_bin = re_encode(value, all_dicts)
-            # when ä occurs (aL\n)
-            if repaired_bin.find(b'aL\n') != -1:
-                temp_byte = re_encode(repaired_bin, corrupted_char)
-                continue
-            if temp_byte:
-                repaired_bin = temp_byte + repaired_bin
-            temp_byte = b''
-            repaired_string = repaired_bin.decode('UTF-8')
-            csv_file_io = StringIO(repaired_string)
-            csv_reader = csv.reader(csv_file_io, delimiter=';')
-            saved = write_line_to_csv(list(csv_reader)[0])
-            if saved:
-                row_converted += 1
-            else:
-                error_counter += 1
-    show_result(row_converted, error_counter,file_output)
+        repaired_string = repaired_bin.decode('UTF-8')
+        csv_file_io = StringIO(repaired_string)
+        csv_reader = csv.reader(csv_file_io, delimiter=';')
+        csv_row = list(csv_reader)[0]
+        saved = write_line_to_csv(csv_row)
+        if saved:
+            row_converted += 1
+        else:
+            error_counter += 1
+
+    show_result(row_converted, error_counter, file_output)
 
 
-def show_result(row_converted, error_counter,file_output):
-    print('Script ended')
-    print('Row converted: {}'.format(row_converted))
-    print('----')
-    print('File saved to: {}'.format(file_output))
-    print('----')
-
+def show_result(row_converted, error_counter, file_output):
+    logger.info('Script ended')
+    logger.info('Row converted: {}'.format(row_converted))
+    logger.info('File saved to: {}'.format(file_output))
     if error_counter:
-        print('{} Errors'.format(error_counter))
+        logger.error('{} Errors - check log file'.format(error_counter))
 
-def show_input(filepath):
-    print('----')
-    print('File to convert: {}'.format(filepath))
-    print('----')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert corrupted csv from MN Krakow')
@@ -101,3 +116,4 @@ if __name__ == '__main__':
 
     if args.input:
         csv_converter(args.input)
+
